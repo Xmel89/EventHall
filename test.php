4f93ffabc2beb29178bb92943cce6ed75f731099
Xml <systemoperation@mail.ru>
2016-12-05 14:42:27 +0200

    Сделаны формы

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
-	<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset='utf-'>
@@ -9,29 +9,6 @@
 		<header>
 			<h1>Концертный зал</h1>
 		</header>
-		<table cellpadding='14'>
-			<tr>
-				<td rowspan='4'>
-					<h2><?php echo"$near_date[name]"?></h2>
-					<div class = "imgCenter"><?php echo "<img src='$img_src', alt='картинка с изображением'></img>";?></div>
-				</td>
-				<td><?php echo"$near_date[date] в "; echo"$time"; ?>  </td>
-			</tr>
-			<tr>
-				<td>Цены на билет:</br> от <?php echo"$near_date[t_low]"?> руб </td>
-			</tr>
-			<tr>
-				<td>до <?php echo"$near_date[t_high]" ?> руб</td>
-			</tr>
-			<tr>
-				<td>
-				<a href='buy.html' name='b_buy'>Купить билет</a>
-				</td>
-			</tr>
-		</table>
-	</body>	
-</html>
-
 <?php 
 try{
 	$pdo = new PDO ("mysql:dbname=Hall;host=127.0.0.1:3306", "root", "");
@@ -48,31 +25,32 @@ $query = $pdo->query("SELECT COUNT(*) FROM event");
 	$near_event = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
 	$i=0;
 	while($i<=4){
-$near_date = $near_event[i];
+$near_date = $near_event[$i];
 $img_src = '/img/'.$near_date[date].$near_date[time];
 $img_src =substr("$img_src",0,17);
 $time= substr("$near_date[time]",0,5);
 echo "<table cellpadding='14'>
 			<tr>
 				<td rowspan='4'>
-					<h2> <?php echo '$near_date[name]'?></h2>
-					<div class = 'imgCenter'><?php echo '<img src='$img_src', alt='картинка с изображением'></img>';?></div>
+					<h2>{$near_date['name']}</h2>
+					<div class = 'imgCenter'><img src='{$img_src}', alt='картинка с изображением'></img></div>
 				</td>
-				<td><?php echo'$near_date[date] в '; echo'$time'; ?>  </td>
+				<td>{$near_date['date']} в {$time} </td>
 			</tr>
 			<tr>
-				<td>Цены на билет:</br> от <?php echo'$near_date[t_low]'?> руб </td>
+				<td>Цены на билет:</br> от {$near_date['t_low']} руб </td>
 			</tr>
 			<tr>
-				<td>до <?php echo'$near_date[t_high]' ?> руб</td>
+				<td>до {$near_date['t_high']} руб</td>
 			</tr>
 			<tr>
 				<td>
 				<a href='buy.html' name='b_buy'>Купить билет</a>
 				</td>
 			</tr>
-		</table>";
-
-$i++;		
+		</table>
+	</body>	
+</html>";
+$i++;
 }
 ?>
