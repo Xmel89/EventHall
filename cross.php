d6fd916bb95958b32412ed3b2fa9a8f0b7f92e82
Xml <systemoperation@mail.ru>
2016-12-27 13:27:38 +0200

    add changes in logic 'canceled reserv place'

--- cross.php
+++ cross.php
@@ -1,5 +1,5 @@
 <?php
-//скрипт для удаления неоплаченных концертов 
+//script for delete reserve place
 try{
 	$pdo = new PDO ("mysql:dbname=Hall;host=127.0.0.1:3306", "root", "");
 	$pdo->exec('SET NAMES "utf8"');
@@ -8,11 +8,12 @@ try{
 catch(PDOException $e){
 	echo "Возникла ошибка соединения с БД ".$e->getMessage();
 	exit();
-} //if(fgj@er*10%10==0){echo "yes";}
+} 
 $query ="SELECT * FROM `ev_hall` WHERE datetime>=CURRENT_DATE AND engaged!=0";
 $event = $pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
 $i=0;
-$time = date("Y-m-d H:i:s");
+$H = date('H')-2;// time rush real time on one hour, so two hours ago 
+$time = date("Y-m-d {$H}:i");
 while($i<=count($event)){
 	$datetime = $event[$i][datetime];
 	$engaged = explode('/', $event[$i][engaged]);
@@ -24,13 +25,14 @@ while($i<=count($event)){
 					$key_free = array_search($value+0.1, $free);
 					$free[$key_free]-=0.1;
 					unset($engaged[$key],$engaged[$key+1],$engaged[$key+2]);
-					$engaged = implode('/',$engaged);
-					$free = implode('/',$free);
-					$request = $pdo->query("UPDATE ev_hall SET engaged='$engaged', free='$free' WHERE datetime='$datetime';");
+					prev($engaged);
 				}
 			}
 		}	
 	}
 	$i++;
+	$engaged = implode('/',$engaged);
+	$free = implode('/',$free);
+	$request = $pdo->query("UPDATE ev_hall SET engaged='$engaged', free='$free' WHERE datetime='$datetime';");
 }
 ?>
